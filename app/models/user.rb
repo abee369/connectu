e012b4009bd6f6aebbe0c987f79b36e396469717
abee369 <abee369@hotmail.com>
2015-10-12 18:19:25 -0500

    sign up form corrected

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, uniqueness: true
+  validates :phone, presence: true,numericality: true, length: { :minimum => 10, :maximum => 15 }
   validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
 
